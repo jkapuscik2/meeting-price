36cb4d14d7a5d154b4308059f87236897248ea81
kuba <j.kapuscik@abyssglass.pl>
2019-05-11 22:50:18 +0200

    Updates

---
+++ precache-manifest.2cd456c1979cde8e273922b0ac7bfc82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65349a765230fc93a5924e12e2092217",
+    "url": "/meeting-price/index.html"
+  },
+  {
+    "revision": "73dcf356325b91c33f0b",
+    "url": "/meeting-price/static/css/main.7526125b.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/meeting-price/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "73dcf356325b91c33f0b",
+    "url": "/meeting-price/static/js/main.827506cf.chunk.js"
+  },
+  {
+    "revision": "af63da029f25b6f37801",
+    "url": "/meeting-price/static/js/runtime~main.56ac14d1.js"
+  }
+]);
